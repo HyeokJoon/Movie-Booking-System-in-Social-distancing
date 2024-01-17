e9934b53912d93a43ed629b78f246a6012333787
kimhyeokjoon <fufu98@naver.com>
2024-01-17 12:02:03 +0900

    [docs] List 기능 구현

---
+++ List.cpp
@@ -0,0 +1,20 @@
+#include "List.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+//#include "Profile.h"
+//#include "Movie.h"
+//#include "Statistics.h"
+using namespace std;
+
+// 기본 생성자
+List::List() {
+	sex = "  ";
+	name = "  ";
+	phoneNum = "  ";
+	Theater = "  ";
+	seat = "  ";
+	title = "  ";
+	time = "  ";
+}
